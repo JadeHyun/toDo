8c5aad04bff44641ba2e941d00082d2097ea4dbe
Hyunjun <96432772+JadeHyun@users.noreply.github.com>
2022-08-22 21:43:54 +0900

    redux로 상태 올리기 완성 (#7)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+
+const Button = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #61dafb;
+  font-size: 36px;
+  border: 0;
+  border-radius: 50%;
+  width: 4rem;
+  height: 4rem;
+  margin: 1rem;
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 1rem;
+`;
 
 function App() {
+  const count = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>{count}</span>
+        <ButtonWrapper>
+          <Button
+            onClick={() => {
+              dispatch({ type: 'INCREASE' });
+            }}
+          >
+            +
+          </Button>
+          <Button
+            onClick={() => {
+              dispatch({ type: 'DECREASE' });
+            }}
+          >
+            -
+          </Button>
+        </ButtonWrapper>
       </header>
     </div>
   );
